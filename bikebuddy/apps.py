f7e2afe903c9d960c46a43d82cd6613dcc366a21
Christopher Boland <2193014b@student.gla.ac.uk>
2017-11-17 14:21:50 +0000

    2nd commit to the repo, should include all files :w

---
+++ bikebuddy/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BikebuddyConfig(AppConfig):
+    name = 'bikebuddy'
---
+++ bikebuddy/urls.py
@@ -0,0 +1,4 @@
+from django.conf.urls import patterns, url, include
+from bikebuddy import views
+
+urlpatterns = patterns('', url(r'^$', views.index, name='index'))
